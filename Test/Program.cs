242c9ce22661392b87abb80ab3bd4c94b9995b70
AsteroidPizza39 <45804670+AsteroidPizza39@users.noreply.github.com>
2023-06-25 21:38:25 -0400

    Add source file

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cead;
+using Cead.Handles;
+using Cead.Interop;
+
+DllManager.LoadCead();
+
+string path = args[0];
+Console.WriteLine(path);
+
+using FileStream fs = File.OpenRead(path);
+Span<byte> buffer = new byte[fs.Length];
+fs.Read(buffer);
+
+Byml byml = Byml.FromBinary(buffer);
+Byml.Array array = byml.GetHash()["PathList"].GetArray();
+
+// - Work/Actor/
+// - AirWallStopFlow
+// - .engine__actor__ActorParam.gyml
+
+array.Add("Work/Actor/");
+array.Add(args[1]);
+array.Add(".engine__actor__ActorParam.gyml");
+
+using DataHandle data = byml.ToBinary(false);
+File.WriteAllBytes(args[2], data.ToArray());
